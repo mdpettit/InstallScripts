668556cf344468110f9fd402af0bb7f9255759dd
mdpettit <marshall.pettit@healthcatalyst.com>
2017-03-03 11:53:27 -0700

    Start mirth and tail

--- startmirthandtail.sh
+++ startmirthandtail.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 /opt/mirthconnect/mcservice start
+
+echo "sleeping until mirth connect is running"
+until [ "`/opt/mirthconnect/mcservice status`"=="The daemon is running." ]; do sleep 1s; done;
+
 tail -f /opt/mirthconnect/logs/mirth.log
