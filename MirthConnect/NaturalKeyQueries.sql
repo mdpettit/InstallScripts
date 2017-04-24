d15386dd59ee4008df1f40aec80fedb65f7f7379
mdpettit <marshall.pettit@healthcatalyst.com>
2017-04-24 11:41:09 -0600

    Add discrepancy code management
    Add discrepancy code management to duplicate key handler

--- MirthConnect/NaturalKeyQueries.sql
+++ MirthConnect/NaturalKeyQueries.sql
@@ -1,33 +1,69 @@
-select top 10 * from mirthsource.x12.interchangebase where isa05 = 'ZZ' and isa06 = 'B00099996303' and isa07 = 'ZZ' and isa08 = 'CLAIMLYNX' and isa13  = '001474409'  order by isa13
 
-select ISA05, ISA06, ISA07, ISA08, ISA13, count(ISA05) cnt, max(interchangeid)
-from mirthsource.x12.InterchangeBASE 
-group by ISA05, ISA06, ISA07, ISA08, ISA13
-having count(ISA05) > 1
-go
+--reset the entire collection
+--update mirthsource.x12.interchangebase set discrepancycd = null, DiscrepancyDSC = null
+--update mirthsource.x12.functionalgroupbase set discrepancycd = null, discrepancydsc = null
+--update mirthsource.x12835.transactionbase set discrepancycd = null, DiscrepancyDSC = null
 
---begin tran
---delete mirthsource.x12.InterchangeBASE where isa05 = 'ZZ' and isa06 = 'ENS_EDI' and isa07 = 'ZZ' and isa08 = 'GATE0035' and isa13  = '001411151' and InterchangeID < 3983
+-- Handle Interchange Duplicates
+with interchange_dup (rownum, interchangeID) as (select row_number() over ( partition by i.ISA05, i.ISA06, i.ISA07, i.ISA08, i.ISA13 order by i.loaddts) as rownum, i.InterchangeID
+from mirthsource.x12.InterchangeBASE i)
+update i
+set i.discrepancycd = 1
+from mirthsource.x12.interchangebase i
+join interchange_dup d on i.interchangeid = d.interchangeid
+where d.rownum > 1
 
---rollback
-
-declare @isa05 varchar(255) = '27', 
-@isa06 varchar(255) = '17003', 
-@isa07 varchar(255) = '29', 
-@isa08 varchar(255) = 'B08230369', 
---@isa09 varchar(255) = '140109',
---@isa10 varchar(255) = '0513' ,
-@isa13 varchar(255) = '000000001', 
-@gs06 varchar(255) = '1', 
-@st02 varchar(255) = '0001', 
-@lx01 varchar(255) = '1',
-@clp01 varchar(255) = '032817969854',
-@clp07 varchar(255) = '13347729375000';
---select * from x12.interchangebase where isa05 = @isa05 and isa06 = @isa06 and isa07 = @isa07 and isa08 = @isa08 and isa09 = @isa09 and isa10 = @isa10 and isa13 = @isa13 order by loaddts
---select * from x12.functionalgroup where isa05 = @isa05 and isa06 = @isa06 and isa07 = @isa07 and isa08 = @isa08 and isa09 = @isa09 and isa10 = @isa10 and isa13 = @isa13 order by gs06, loaddts
---select * from x12835.[transaction] where isa05 = @isa05 and isa06 = @isa06 and isa07 = @isa07 and isa08 = @isa08 and isa09 = @isa09 and isa10 = @isa10 and isa13 = @isa13 order by gs06, st02, loaddts
---select * From x12835.header where isa05 = @isa05 and isa06 = @isa06 and isa07 = @isa07 and isa08 = @isa08 and isa09 = @isa09 and isa10 = @isa10 and isa13 = @isa13 order by gs06, st02, lx01, loaddts
-select * from x12835.claim where isa05 = @isa05 and isa06 = @isa06 and isa07 = @isa07 and isa08 = @isa08  and isa13 = @isa13 order by gs06, st02, lx01, clp01, clp02, loaddts
-go
+-- Handle Functional Group Duplicates
+with functional_dup (rownum, InterchangeID, functionalgroupid, ISA05, ISA06, ISA07, ISA08, ISA13, GS06) as (SELECT
+row_number() over (partition by ISA05, ISA06, ISA07, ISA08, ISA13, GS06 order by i.loaddts) as rownum,
+i.InterchangeID, functionalgroupid, ISA05, ISA06, ISA07, ISA08, ISA13, GS06
+FROM MirthSource.X12.FunctionalGroupBASE f
+JOIN MirthSource.X12.InterchangeBASE i ON f.InterchangeID = i.InterchangeID
+where i.DiscrepancyCD is null or i.DiscrepancyCD not in (1)
+) 
+--select count(*) from functional_dup where rownum > 1
+update f
+set f.discrepancycd = 2, f.DiscrepancyDSC = isnull(f.DiscrepancyDSC,'') + 'Duplicate functional group: ISA05 [' + d.ISA05 + '], ISA06 ' + d.ISA06 + ', ISA07 ' + d.ISA07 + ', ISA08 ' + d.ISA08 + ', ISA13 ' + d.ISA13 + ', GS06 ' + d.GS06 + ', ' 
+from mirthsource.x12.functionalgroupbase f
+join functional_dup d on f.functionalgroupid = d.functionalgroupid
+where d.rownum > 1
 
 　
+-- Handle Transaction Duplicates
+with transaction_dup (rownum, interchangeid, transactionid) as (select
+row_number() over (partition by isa05, isa06, isa07, isa08, isa13, gs06, st02 order by i.loaddts) as ronum,
+i.interchangeid, t.transactionid
+from MirthSource.X12835.TransactionBASE t
+JOIN MirthSource.X12.FunctionalGroupBASE f ON t.FunctionalGroupID = f.FunctionalGroupID
+JOIN MirthSource.X12.InterchangeBASE i ON f.InterchangeID = i.InterchangeID
+where i.DiscrepancyCD is null or i.DiscrepancyCD not in (1,2)
+)
+--select count(*) from transaction_dup where rownum > 1
+update t
+set DiscrepancyCD = 3, DiscrepancyDSC = ISNULL(DiscrepancyDSC, '') + 'Duplicate transaction: ST02 [' + st02 + ']; '
+from mirthsource.x12835.transactionbase t
+join transaction_dup d on t.transactionid = d.transactionid
+where rownum > 1
+
+　
+-- Handle Claim Duplicates
+with claim_dup (rownum, interchangeid, transactionid, lx01, clp01, clp02, clp07) as (select
+row_number() over (partition by isa05, isa06, isa07, isa08, isa13, gs06, st02, lx01, clp01, clp02, clp07 order by i.loaddts) as rownum
+, i.interchangeid, t.transactionid, lx01, clp01, clp02, clp07
+from MirthSource.X12835.ClaimBASE c
+JOIN MirthSource.X12835.HeaderBASE h ON c.HeaderID = h.HeaderID
+JOIN MirthSource.X12835.TransactionBASE t ON h.TransactionID = t.TransactionID
+JOIN MirthSource.X12.FunctionalGroupBASE f ON t.FunctionalGroupID = f.FunctionalGroupID
+JOIN MirthSource.X12.InterchangeBASE i ON f.InterchangeID = i.InterchangeID
+where i.DiscrepancyCD is null or i.DiscrepancyCD not in (1,2,3)
+)
+--select * from claim_dup where rownum > 1
+update t 
+set DiscrepancyCD = 4, DiscrepancyDSC = ISNULL(DiscrepancyDSC,'') + 'Duplicate claim: LX01 [' + lx01 + '], CLP01 [' + clp01 + '], CLP02 [' + clp02 + '], CLP07 [' + clp07 + ']; '
+from mirthsource.x12835.transactionbase t
+join claim_dup d on t.transactionid = d.transactionid
+where rownum > 1
+
+update mirthsource.x12.interchangebase set DiscrepancyCD = 0 where DiscrepancyCD is null
+update mirthsource.x12.functionalgroupbase set DiscrepancyCD = 0 where DiscrepancyCD is null
+update mirthsource.x12835.transactionbase set DiscrepancyCD = 0 where DiscrepancyCD is null
